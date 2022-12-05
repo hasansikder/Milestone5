a9688c5171ba4e8aea64576ab27dd9bffc606af3
HasanSikder <mdhasansikder16@gmail.com>
2022-12-05 13:25:37 +0600

    first commit

---
+++ module24/tourOfDoom/js/append.js
@@ -0,0 +1,10 @@
+// const placesList=document.getElementById('places-list');
+// const li =document.createElement('li');
+// li.innerText='United state';
+
+// placesList.appendChild(li)
+
+const add = document.getElementById('aboutSection');
+const li = document.createElement('li');
+li.innerText='Contact us ';
+add.appendChild(li)
---
+++ module24/tourOfDoom/js/doom.js
@@ -0,0 +1,15 @@
+// let myele= document.getElementsByClassName('abuls')
+// for (let my of myele){
+//     console.log(my);
+// }
+
+const section = document.getElementById('firstSection');
+section.style.color = "red";
+section.style.backgroundColor = "black";
+section.style.width = '100px';
+section.style.marginLeft = '100px';
+
+const txt= document.querySelector('firstSection') ;
+console.log(txt)
+
+
